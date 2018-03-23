d53c4589562d41cac8fc8cf1ac686e305c903426
Hilton <hdcs05@gmail.com>
2018-03-23 19:14:19 -0400

    Changed labels on displayMessage Method

--- app/src/main/java/com/example/android/justjava2/MainActivity.java
+++ app/src/main/java/com/example/android/justjava2/MainActivity.java
@@ -106,8 +106,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView summaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        summaryTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
 }
